847a3b5e3a0b78809d24dd6408aebde454c1f07b
JosephKS10 <jjbrothersdelhi@hotmail.com>
2025-04-03 18:06:21 +1100

    remove node_modules

--- Workshop-Week4/index.js
+++ Workshop-Week4/index.js
@@ -2,6 +2,7 @@ var express = require("express")
 var app = express()
 var port = process.env.port || 3000;
 let cors = require("cors");
+const mongoose = require('mongoose');
 
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
@@ -39,4 +40,11 @@ var port = 5500;
 app.listen(port,()=>{
 console.log("App listening to: "+port)
 })
+
+mongoose.connect('mongodb://localhost:27017/myprojectDB');
+
+mongoose.connection.on('connected', () => {
+        console.log('Connected to MongoDB!');
+        });
+        
     
